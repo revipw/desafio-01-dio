66adaebb8412e4ddade63788a859329c41c1a0fa
revipw <renatojustino.edu@gmail.com>
2024-02-19 14:52:28 -0300

    Commit 2

--- ex01.js
+++ ex01.js
@@ -1,6 +1,23 @@
-function sum(value){
-    return 1+1;
+function calculate(wins, loses){
+    return (wins-loses);
+}
+let LP = calculate(80, 35);
+let elo;
+if(LP <= 10){
+    elo = 'Ferro'
+}else if(LP >= 11 && LP <= 20){
+    elo =  'Bronze'
+}else if(LP >= 21 && LP <= 50){
+   elo =  'Prata'
+}else if(LP >= 51 && LP <= 80){
+    elo =  'Ouro'
+}else if(LP >= 81 && LP <= 90){
+   elo =  'Diamante'
+}else if(LP >= 91 && LP <= 100){
+   elo =  'Lendário'
+}else{
+   elo =  'Imortal'
 }
 
-let value = sum(value);
-console.log(value);
+console.log("The Hero has " + LP + " LP and it's on level " + elo +"!");
+ 
